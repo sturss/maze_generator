2c264d1e6b205c1a45590ac635b474aa46dbdf8d
sturss <sturss22@gmail.com>
2017-11-09 18:55:23 +0200

    - maze is not considered created until all graphical transitions are fished.

--- com/sviatoslav/mazes/DepthFirstMazeAlgorithm.java
+++ com/sviatoslav/mazes/DepthFirstMazeAlgorithm.java
@@ -10,18 +10,25 @@ import java.util.Random;
 import java.util.Stack;
 
 class DepthFirstMazeAlgorithm implements MazeAlgorithm {
+    private int iterationCounter = 0;
+    private int transitionCounter = 0;
+    private boolean isDone;
     @Override
-    public void createMaze(Cell[][] cells, int rows, int cols) {
+    public void createMaze(Maze maze) {
         Random rand = new Random();
         Stack<Point> stack = new Stack<>();
         Side side;
         ArrayList<Side> sides = new ArrayList<>(4);
-        int row = rand.nextInt(rows - 2) + 1;
+        int row = rand.nextInt(maze.getRows() - 2) + 1;
         int col = 0;
-        cells[row][col].getVisited();
-        cells[row][col].deleteWall(Side.LEFT_SIDE);
-        cells[row][col].setVisitedColor();
-        cells[rand.nextInt(rows - 1)][cols - 1].deleteWall(Side.RIGHT_SIDE);
+
+        maze.setEnter(row ,col);
+        maze.getEnter().getVisited();
+        maze.getEnter().deleteWall(Side.LEFT_SIDE);
+        maze.getEnter().setVisitedColor();
+        maze.setExit(rand.nextInt(maze.getRows() - 1), maze.getCols() - 1);
+        maze.getExit().deleteWall(Side.RIGHT_SIDE);
+
         stack.push(new Point(row, col));
         long time = 20;
         while (true) {
@@ -29,9 +36,10 @@ class DepthFirstMazeAlgorithm implements MazeAlgorithm {
             PauseTransition visitFrom1 = new PauseTransition(Duration.millis(time));
             PauseTransition getVisited1 = new PauseTransition(Duration.millis(time));
 
-            while (!checkSides(cells, rows, cols, sides, row, col)){
+            while (!checkSides(maze, sides, row, col)){
                 stack.pop();
                 if (stack.isEmpty()) {
+                    isDone =true;
                     return;
                 }
                 row = stack.lastElement().x;
@@ -42,7 +50,11 @@ class DepthFirstMazeAlgorithm implements MazeAlgorithm {
 
             int[] args = {row, col};
             Side s = side;
-            visitFrom1.setOnFinished(event -> cells[args[0]][args[1]].deleteWall(s));
+
+            visitFrom1.setOnFinished(event -> {
+                maze.getCell(args[0], args[1]).deleteWall(s);
+                maze.getCell(args[0], args[1]).update();
+            });
 
             if (side == Side.TOP_SIDE) {
                 row -= 1;
@@ -56,30 +68,36 @@ class DepthFirstMazeAlgorithm implements MazeAlgorithm {
             stack.push(new Point(row, col));
 
             int[] args2 = {row, col};
-            cells[row][col].getVisited();
+            maze.getCell(row, col).getVisited();
             getVisited1.setOnFinished(event -> {
-                cells[args2[0]][args2[1]].deleteWall(Side.from(3-s.getValue()));
-                cells[args2[0]][args2[1]].setVisitedColor();
-        });
+                maze.getCell(args2[0], args2[1]).deleteWall(Side.from(3-s.getValue()));
+                maze.getCell(args2[0], args2[1]).setVisitedColor();
+                maze.getCell(args2[0], args2[1]).update();
+                transitionCounter += 1;
+                if(isDone && iterationCounter == transitionCounter) {
+                    maze.setCreated(true);
+                }
+            });
 
             sides.clear();
+            iterationCounter += 1;
             ParallelTransition transitions = new ParallelTransition(visitFrom1, getVisited1);
             transitions.play();
         }
     }
 
-    private boolean checkSides(Cell[][] cells, int rows, int cols, ArrayList<Side> sides, int row, int col) {
+    private boolean checkSides(Maze maze, ArrayList<Side> sides, int row, int col) {
         if(col > 0)
-            if(!cells[row][col-1].isVisited())
+            if(!maze.getCell(row, col-1).isVisited())
                 sides.add(Side.LEFT_SIDE);
         if(row > 0)
-            if(!cells[row-1][col].isVisited())
+            if(!maze.getCell(row-1, col).isVisited())
                 sides.add(Side.TOP_SIDE);
-        if(col < cols -1)
-            if(!cells[row][col+1].isVisited())
+        if(col < maze.getCols() -1)
+            if(!maze.getCell(row, col+1).isVisited())
                 sides.add(Side.RIGHT_SIDE);
-        if(row < rows -1)
-            if(!cells[row+1][col].isVisited())
+        if(row < maze.getRows() -1)
+            if(!maze.getCell(row+1, col).isVisited())
                 sides.add(Side.BOTTOM_SIDE);
         return !sides.isEmpty();
     }
--- com/sviatoslav/mazes/RecursiveDivisionMazeAlgorithm.java
+++ com/sviatoslav/mazes/RecursiveDivisionMazeAlgorithm.java
@@ -8,62 +8,85 @@ import java.util.Random;
 
 class RecursiveDivisionMazeAlgorithm implements MazeAlgorithm {
     private int time;
+    private int functionCounter = 0;
+    private int transitionCounter = 0;
+    private boolean isDone = false;
     @Override
-    public void createMaze(Cell[][] cells, int rows, int cols) {
-        time = 50;
-        for (int i = 0; i < rows; i++) {
-            for(int j =0; j < cols; j++) {
-                if (i < rows-1) {
-                    cells[i][j].deleteWall(Side.BOTTOM_SIDE);
-                    cells[i+1][j].deleteWall(Side.TOP_SIDE);
-                }
-                if (j < cols-1) {
-                    cells[i][j].deleteWall(Side.RIGHT_SIDE);
-                    cells[i][j+1].deleteWall(Side.LEFT_SIDE);
-                }
-                cells[i][j].setVisitedColor();
-            }
-        }
-        createM(cells, 0, 0, cols-1, rows-1, 0);
+    public void createMaze(Maze maze) {
+        time = 10;
+        maze.destroyWalls();
+        createM(maze, 0, 0, maze.getCols()-1,
+                maze.getRows()-1, 0);
+
         Random random = new Random();
-        cells[random.nextInt(rows-1)][0].deleteWall(Side.LEFT_SIDE);
-        cells[random.nextInt(rows-1)][cols-1].deleteWall(Side.RIGHT_SIDE);
+        maze.setEnter(random.nextInt(maze.getRows()-1), 0);
+        maze.getEnter().deleteWall(Side.LEFT_SIDE);
+        maze.getEnter().update();
+
+        maze.setExit(random.nextInt(maze.getRows()-1), maze.getCols()-1);
+        maze.getExit().deleteWall(Side.RIGHT_SIDE);
+        maze.getExit().update();
+
+        isDone = true;
     }
 
-    private void createM(Cell[][] cells, int leftColBound, int topRowBound,
+    private void createM(Maze maze, int leftColBound, int topRowBound,
                          int rightColBound, int bottomRowBound, int turn) {
         if(rightColBound - leftColBound >= 1 && bottomRowBound - topRowBound >= 1) {
-            this.time += 200;
+            this.time += 100;
             Random rand = new Random();
-            PauseTransition drawingPause  = new PauseTransition(Duration.millis(time));
+            PauseTransition buildingPause  = new PauseTransition(Duration.millis(time));
             int row = topRowBound + rand.nextInt(bottomRowBound - topRowBound + turn) ;
             int col = leftColBound + rand.nextInt(rightColBound- leftColBound + 1 - turn) ;
 
             if(turn == 0) {
-                drawingPause.setOnFinished(event -> {
+                buildingPause.setOnFinished(event -> {
+                    transitionCounter += 1;
                     for (int i = leftColBound; i <= rightColBound; i++) {
-                        cells[row][i].buildWall(Side.BOTTOM_SIDE);
-                        cells[row + 1][i].buildWall(Side.TOP_SIDE);
+                        maze.getCell(row, i).buildWall(Side.BOTTOM_SIDE);
+                        maze.getCell(row+1, i).buildWall(Side.TOP_SIDE);
                     }
-                    cells[row][col].deleteWall(Side.BOTTOM_SIDE);
-                    cells[row + 1][col].deleteWall(Side.TOP_SIDE);
+                    maze.getCell(row, col).deleteWall(Side.BOTTOM_SIDE);
+                    maze.getCell(row+1, col).deleteWall(Side.TOP_SIDE);
+                    for (int i = leftColBound; i <= rightColBound; i++) {
+                        maze.getCell(row, i).update();
+                        maze.getCell(row + 1, i).update();
+                    }
+                    maze.getCell(row, col).update();
+                    maze.getCell(row+1, col).update();
+                    if(isDone && functionCounter == transitionCounter) {
+                        maze.setCreated(true);
+                    }
                 });
-                drawingPause.play();
-                createM(cells, leftColBound, topRowBound, rightColBound, row, 1);
-                createM(cells, leftColBound, row+1, rightColBound, bottomRowBound, 1);
+                functionCounter += 1;
+                buildingPause.play();
+                createM(maze, leftColBound, topRowBound, rightColBound, row, 1);
+                createM(maze, leftColBound, row+1, rightColBound, bottomRowBound, 1);
             }
             else {
-                drawingPause.setOnFinished(event -> {
-                            for (int i = topRowBound; i <= bottomRowBound; i++) {
-                                cells[i][col].buildWall(Side.RIGHT_SIDE);
-                                cells[i][col + 1].buildWall(Side.LEFT_SIDE);
-                            }
-                            cells[row][col].deleteWall(Side.RIGHT_SIDE);
-                            cells[row][col+1].deleteWall(Side.LEFT_SIDE);
-                        });
-                drawingPause.play();
-                createM(cells, leftColBound, topRowBound, col, bottomRowBound, 0);
-                createM(cells, col+1, topRowBound, rightColBound, bottomRowBound, 0);
+                buildingPause.setOnFinished(event -> {
+                    transitionCounter += 1;
+                    for (int i = topRowBound; i <= bottomRowBound; i++) {
+                        maze.getCell(i, col).buildWall(Side.RIGHT_SIDE);
+                        maze.getCell(i, col + 1).buildWall(Side.LEFT_SIDE);
+                    }
+                    maze.getCell(row, col).deleteWall(Side.RIGHT_SIDE);
+                    maze.getCell(row, col+1).deleteWall(Side.LEFT_SIDE);
+
+                    for (int i = topRowBound; i <= bottomRowBound; i++) {
+                        maze.getCell(i, col).update();
+                        maze.getCell(i, col + 1).update();
+                    }
+                    maze.getCell(row, col).update();
+                    maze.getCell(row, col+1).update();
+                    if(isDone && functionCounter == transitionCounter) {
+                        maze.setCreated(true);
+                    }
+                });
+                functionCounter += 1;
+                buildingPause.play();
+                createM(maze, leftColBound, topRowBound, col, bottomRowBound, 0);
+                createM(maze, col+1, topRowBound, rightColBound, bottomRowBound, 0);
             }
         }
     }
