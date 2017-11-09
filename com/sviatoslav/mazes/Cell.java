ee321ac636306f623c435e6712f74340902f95c3
sturss <sturss22@gmail.com>
2017-11-09 18:50:41 +0200

    - function update() became package-private and deleted all occurrences of calling it from other functions.  - size of numbers reduced.

--- com/sviatoslav/mazes/Cell.java
+++ com/sviatoslav/mazes/Cell.java
@@ -1,6 +1,9 @@
 package sviatoslav.mazes;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import sviatoslav.enums.Side;
 
 class Cell extends Region {
@@ -8,26 +11,26 @@ class Cell extends Region {
     private boolean visited = false;
     private String colorStyle= "-fx-background-color: #00766c; ";
     private String borderStyle = "-fx-border-width: 2 2 2 2; -fx-border-color: #007c91; ";
+    private int length;
 
     Cell(int x, int y) {
-        setMinWidth(x);
-        setMinHeight(y);
+        setPrefSize(x, y);
+        length = 0;
         update();
     }
 
-    private void update() {
+    void update() {
         borderStyle = "-fx-border-width:" +
-                2*walls[Side.TOP_SIDE.getValue()] + " " +
-                2*walls[Side.RIGHT_SIDE.getValue()] + " " +
-                2*walls[Side.BOTTOM_SIDE.getValue()] + " " +
-                2*walls[Side.LEFT_SIDE.getValue()] + "; " +
+                walls[Side.TOP_SIDE.getValue()] + " " +
+                walls[Side.RIGHT_SIDE.getValue()] + " " +
+                walls[Side.BOTTOM_SIDE.getValue()] + " " +
+                walls[Side.LEFT_SIDE.getValue()] + "; " +
                 "-fx-border-color: #007c91;";
         setStyle(colorStyle + borderStyle);
     }
 
     void deleteWall(Side side) {
         walls[side.getValue()] = 0;
-        update();
     }
 
     void getVisited() {
@@ -36,16 +39,40 @@ class Cell extends Region {
 
     void buildWall(Side side) {
         walls[side.getValue()] = 1;
-        update();
     }
 
     void setVisitedColor() {
         colorStyle = "-fx-background-color: #52c7b8; ";
-        update();
+    }
+
+    void setUnvisitedColor() {
+        colorStyle = "-fx-background-color: #00766c; ";
+    }
+
+
+    int getLength() {
+        return length;
+    }
+
+    void setLength(int length) {
+        this.length = length;
+    }
+
+    void drawLength() {
+        Label lengthLabel = new Label();
+        lengthLabel.setText(String.valueOf(this.length));
+        lengthLabel.setStyle("-fx-font: " + this.getPrefHeight()  / 2 + " ariel; -fx-text-fill: #00766c");
+        StackPane stack = new StackPane(lengthLabel);
+        stack.setPrefSize(getPrefWidth(), getPrefHeight());
+        stack.setAlignment(Pos.TOP_CENTER);
+        getChildren().add(stack);
     }
 
     boolean isVisited() {
         return visited;
     }
 
+    boolean isWall(Side side) {
+        return walls[side.getValue()] == 1;
+    }
 }
